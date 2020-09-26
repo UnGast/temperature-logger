287703aa019c79da302fab5630a5c7029852d6cf
UnGast <zadrian@web.de>
2020-09-26 11:52:58 +0200

    add typescript

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,44 +1,27 @@
 <template>
-  
-  <section class="app">
-
-    <div style="display:none" v-html="iconSvg"></div>
-
-    <dashboard/>
-
-  </section>
-
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import iconSvg from '!raw-loader!~/assets/icons/icomoon/symbol-defs.svg'
-import Dashboard from './components/Dashboard.vue'
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
+@Options({
   components: {
-    Dashboard
+    HelloWorld,
   },
-  computed: {
-    iconSvg() {
-      return iconSvg
-    }
-  }
-}
+})
+export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-@import "~/assets/style/global/index.scss";
-</style>
-
-<style lang="scss" scoped>
-.app {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-
-.dashboard {
-  flex-grow: 1;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
