14da68752570ab6919cace8fb18324544294e20a
UnGast <zadrian@web.de>
2021-07-22 19:44:05 +0200

    start testing notifications

--- backend/notification_manager.py
+++ backend/notification_manager.py
@@ -4,119 +4,124 @@ import asyncio
 from threading import Thread
 from pathlib import Path
 import time
-from typing import List, Any, Optional
+from typing import List, Any, Optional, Callable
 import yaml
 from sensor_manager import SensorManager
 from email_manager import EmailManager
 
 class NotificationConfig(ABC):
-	def __init__(self, sender_email: str, receiver_email: str, message_subject: str, message: Optional[str], check_interval: float):
-		self.sender_email = sender_email
-		self.receiver_email = receiver_email
-		self.message_subject = message_subject
-		self.message = message
-		self.check_interval = check_interval
+    def __init__(self, get_time: Callable[[], float], sender_email: str, receiver_email: str, message_subject: str, message: Optional[str], check_interval: float):
+        self.get_time = get_time
+        self.sender_email = sender_email
+        self.receiver_email = receiver_email
+        self.message_subject = message_subject
+        self.message = message
+        self.check_interval = check_interval
 
-	@classmethod
-	@abstractmethod
-	def get_type_name(cls):
-		pass
+    @classmethod
+    @abstractmethod
+    def get_type_name(cls):
+        pass
 
-	@classmethod
-	def from_dict(cls, raw_dict):
-		if raw_dict['type'] != cls.get_type_name():
-			raise Exception('raw dictionary does not seem to encode for this message type')
+    @classmethod
+    def from_dict(cls, raw_dict, get_time):
+        """get_time: function to get current time in seconds from epoch"""
 
-		cls_params = cls.params_from_dict(raw_dict)
+        if raw_dict['type'] != cls.get_type_name():
+            raise Exception('raw dictionary does not seem to encode for this message type')
 
-		if cls_params == False:
-			raise Exception('raw dictionary does not seem to encode for this message type')
-		
-		result = cls(
-			sender_email=raw_dict['sender'],
-			receiver_email=raw_dict['receiver'],
-			message_subject=raw_dict['message_subject'],
-			message=raw_dict['message'] if 'message' in raw_dict else None,
-			check_interval=float(raw_dict['check_interval']),
-			**cls_params)
+        cls_params = cls.params_from_dict(raw_dict)
 
-		return result
+        if cls_params == False:
+            raise Exception('raw dictionary does not seem to encode for this message type')
+        
+        result = cls(
+            get_time=get_time,
+            sender_email=raw_dict['sender'],
+            receiver_email=raw_dict['receiver'],
+            message_subject=raw_dict['message_subject'],
+            message=raw_dict['message'] if 'message' in raw_dict else None,
+            check_interval=float(raw_dict['check_interval']),
+            **cls_params)
 
-	@classmethod
-	@abstractmethod
-	def params_from_dict(cls, raw_dict):
-		pass
+        return result
 
-	@abstractmethod
-	def check_signal(self, current_sensor_values) -> bool:
-		pass
+    @classmethod
+    @abstractmethod
+    def params_from_dict(cls, raw_dict):
+        pass
+
+    @abstractmethod
+    def check_signal(self, current_sensor_values) -> bool:
+        pass
 
 class BaseThresholdNotificationConfig(NotificationConfig):
-	def __init__(self, sensor_id: str, threshold: float, min_breach_duration: float, **kwargs):
-		super().__init__(**kwargs)
-		self.sensor_id = sensor_id
-		self.threshold = threshold
-		self.min_breach_duration = min_breach_duration
-		self.previous_check_value = None
+    def __init__(self, sensor_id: str, threshold: float, min_breach_duration: float, **kwargs):
+        super().__init__(**kwargs)
+        self.sensor_id = sensor_id
+        self.threshold = threshold
+        self.min_breach_duration = min_breach_duration
+        self.previous_check_value = None
 
-		self.tracking_breach = False
-		self.current_breach_start_timestamp = None
-		self.current_breach_notified = False
+        self.tracking_breach = False
+        self.current_breach_start_timestamp = None
+        self.current_breach_notified = False
 
-	@classmethod
-	@abstractmethod
-	def get_type_name(cls):
-		pass
+    @classmethod
+    @abstractmethod
+    def get_type_name(cls):
+        pass
 
-	@classmethod
-	def params_from_dict(cls, raw_dict):
-		return {
-			'sensor_id': raw_dict['sensor'],
-			'threshold': float(raw_dict['threshold']),
-			'min_breach_duration': float(raw_dict['min_breach_duration'])
-		}
+    @classmethod
+    def params_from_dict(cls, raw_dict):
+        return {
+            'sensor_id': raw_dict['sensor'],
+            'threshold': float(raw_dict['threshold']),
+            'min_breach_duration': float(raw_dict['min_breach_duration'])
+        }
 
-	def check_signal(self, current_sensor_values) -> bool:
-		breached = False
-		
-		try:
-			breached = self.check_threshold_breached(current_sensor_values)
-		except Exception as e:
-			breached = self.tracking_breach
+    def check_signal(self, current_sensor_values) -> bool:
+        breached = False
+        
+        try:
+            breached = self.check_threshold_breached(current_sensor_values)
+        except Exception as e:
+            breached = self.tracking_breach
 
-		if breached and not self.tracking_breach:
-			print("start tracking threshold breach", self)
-			self.tracking_breach = True
-			self.current_breach_start_timestamp = time.time()
-			self.current_breach_notified = False
-		elif not breached and self.tracking_breach:
-			print("threshold breach interrupted by non-breaching value", self)
-			self.tracking_breach = False
+        if breached and not self.tracking_breach:
+            print("start tracking threshold breach", self)
+            self.tracking_breach = True
+            self.current_breach_start_timestamp = self.get_time()
+            self.current_breach_notified = False
+        elif not breached and self.tracking_breach:
+            print("threshold breach interrupted by non-breaching value", self)
+            self.tracking_breach = False
 
-		if breached and self.tracking_breach:
-			print("check breach reaches min duration")
-			current_timestamp = time.time()
-			duration = current_timestamp - self.current_breach_start_timestamp
-			duration_reached = duration >= self.min_breach_duration
+        if breached and self.tracking_breach:
+            print("check breach reaches min duration")
+            current_timestamp = self.get_time()
+            duration = current_timestamp - self.current_breach_start_timestamp
+            duration_reached = duration >= self.min_breach_duration
 
-			print("reached?", duration_reached)
+            print("reached?", duration_reached)
 
-			result = duration_reached and not self.current_breach_notified 
-			print("notify?", result, "already notified?", self.current_breach_notified)
+            result = duration_reached and not self.current_breach_notified 
+            print("notify?", result, "already notified?", self.current_breach_notified)
 
-			if duration_reached:
-				self.current_breach_notified = True
+            if duration_reached:
+                self.current_breach_notified = True
+            
+            print("BFORE RET", result)
+            return result
 
-			return result
+        return False
 
-		return False
-	
-	@abstractmethod
-	def check_threshold_breached(self, current_sensor_values):
-		pass
+        @abstractmethod
+        def check_threshold_breached(self, current_sensor_values):
+            pass
 
-	def __str__(self):
-		return f'Notification {{ {self.get_type_name()}: {str(self.threshold)}, sender: {self.sender_email}, receiver: {self.receiver_email} }}'
+        def __str__(self):
+            return f'Notification {{ {self.get_type_name()}: {str(self.threshold)}, sender: {self.sender_email}, receiver: {self.receiver_email} }}'
 
 class FallBelowNotificationConfig(BaseThresholdNotificationConfig):
 	@classmethod
@@ -183,75 +188,77 @@ class SystemStartNotificationConfig(NotificationConfig):
 		return result
 
 class NotificationManager:
-	def __init__(self, notification_configs, sensor_manager: SensorManager, email_manager: EmailManager):
-		self.notification_configs = notification_configs
-		self.sensor_manager = sensor_manager
-		self.email_manager = email_manager
-		self.last_check_timestamps = {}
-		self.current_sensor_values = None
-		self.master_check_interval = min([x.check_interval for x in notification_configs])
-		print('Notification Manager master_check_interval', self.master_check_interval)
-	
-	def start_watching(self):
-		thread = Thread(target=self.watch_loop)
-		thread.start()
-	
-	def watch_loop(self):
-		current_timestamp = time.time()
+    def __init__(self, notification_configs, sensor_manager: SensorManager, email_manager: EmailManager, get_time = time.time):
+        self.notification_configs = notification_configs
+        self.sensor_manager = sensor_manager
+        self.email_manager = email_manager
+        self.get_time = get_time
 
-		self.current_sensor_values = asyncio.run(self.sensor_manager.get_latest_values_filled_with_previous())
+        self.last_check_timestamps = {}
+        self.current_sensor_values = None
+        self.master_check_interval = min([x.check_interval for x in notification_configs])
+        print('Notification Manager master_check_interval', self.master_check_interval)
 
-		try:
-			for index, notification_config in enumerate(self.notification_configs):
-				if not index in self.last_check_timestamps or\
-					self.last_check_timestamps[index] + notification_config.check_interval <= current_timestamp:
-					
-					self.last_check_timestamps[index] = current_timestamp
+    def start_watching(self):
+        thread = Thread(target=self.watch_loop)
+        thread.start()
 
-					try:
-						if notification_config.check_signal(self.current_sensor_values):
-							self.send_notification(notification_config)
-					except Exception as e:
-						print(f'an error occurred while attempting to check or send notification {notification_config}', e)
-		finally:
-				time.sleep(self.master_check_interval)
-				self.watch_loop()
+    def watch_loop(self):
+        current_timestamp = self.get_time()
 
-	def send_notification(self, notification_config: NotificationConfig):
-		print('send notification for config:', notification_config)
-		
-		text = notification_config.message
-		if text is None:
-			text = 'notification was sent automatically'
+        self.current_sensor_values = asyncio.run(self.sensor_manager.get_latest_values_filled_with_previous())
 
-		self.email_manager.send_email(
-			sender_address=notification_config.sender_email,
-			receiver_address=notification_config.receiver_email,
-			subject=notification_config.message_subject,
-			text=text)
-	
-	@staticmethod
-	def parse_notifications_config(config_file_path: Path) -> List[Any]:
-		with open(config_file_path, 'rb') as file:
-			raw_notification_configs = yaml.load(file, Loader=yaml.Loader)
+        try:
+            for index, notification_config in enumerate(self.notification_configs):
+                if not index in self.last_check_timestamps or\
+                    self.last_check_timestamps[index] + notification_config.check_interval <= current_timestamp:
+                    
+                    self.last_check_timestamps[index] = current_timestamp
 
-			potential_config_types = [FallBelowNotificationConfig, RiseAboveNotificationConfig, SystemStartNotificationConfig]
-			notification_configs = []
-			for index, raw_notification_config in enumerate(raw_notification_configs):
-				found_matching_type = False
+                    try:
+                        if notification_config.check_signal(self.current_sensor_values):
+                            self.send_notification(notification_config)
+                    except Exception as e:
+                        print(f'an error occurred while attempting to check or send notification {notification_config}', e)
+        finally:
+                time.sleep(self.master_check_interval)
+                self.watch_loop()
 
-				for potential_type in potential_config_types:
-					try:
-						parsed = potential_type.from_dict(raw_notification_config)
-						if parsed is not None:
-							notification_configs.append(parsed)
-							found_matching_type = True
-							break	
-					except Exception as e:
-						#print(f'error when parsing config {index}: {e}')
-						pass
+    def send_notification(self, notification_config: NotificationConfig):
+        print('send notification for config:', notification_config)
+        
+        text = notification_config.message
+        if text is None:
+            text = 'notification was sent automatically'
 
-				if not found_matching_type:
-					raise Exception(f'found no matching type for notification config at index: {index}')
-					
-			return notification_configs
+        self.email_manager.send_email(
+            sender_address=notification_config.sender_email,
+            receiver_address=notification_config.receiver_email,
+            subject=notification_config.message_subject,
+            text=text)
+
+    @staticmethod
+    def parse_notifications_config(config_file_path: Path) -> List[Any]:
+        with open(config_file_path, 'rb') as file:
+            raw_notification_configs = yaml.load(file, Loader=yaml.Loader)
+
+            potential_config_types = [FallBelowNotificationConfig, RiseAboveNotificationConfig, SystemStartNotificationConfig]
+            notification_configs = []
+            for index, raw_notification_config in enumerate(raw_notification_configs):
+                found_matching_type = False
+
+                for potential_type in potential_config_types:
+                    try:
+                        parsed = potential_type.from_dict(raw_notification_config)
+                        if parsed is not None:
+                            notification_configs.append(parsed)
+                            found_matching_type = True
+                            break	
+                    except Exception as e:
+                        #print(f'error when parsing config {index}: {e}')
+                        pass
+
+                if not found_matching_type:
+                    raise Exception(f'found no matching type for notification config at index: {index}')
+                    
+            return notification_configs
---
+++ backend/test_notification_manager.py
@@ -0,0 +1,42 @@
+from notification_manager import RiseAboveNotificationConfig
+
+class MockEmailManager:
+    def __init__(self, on_send):
+        self.on_send = on_send
+
+    def send_email(self, sender_address, receiver_address, subject, text):
+        self.on_send()
+
+class MockGetTime:
+    def __init__(self):
+        self.time = 0
+
+    def __call__(self):
+        t = self.time
+        self.time += 1
+        return t
+
+def test_rise_above_notification_config():
+    config = RiseAboveNotificationConfig(
+            get_time=MockGetTime(),
+            sender_email='sender',
+            receiver_email='receiver',
+            message_subject='subject',
+            message='message',
+            check_interval=0,
+            sensor_id=0,
+            threshold=0,
+            min_breach_duration=2
+        )
+    
+    assert not config.check_signal({0: -1})
+    assert not config.check_signal({0: -2})
+
+    assert not config.check_signal({0: 0.01})
+    assert config.check_signal({0: 0.01}) == True
+
+    assert not config.check_signal({0: 1})
+    assert not config.check_signal({0: -1})
+
+    assert not config.check_signal({0: 1})
+    assert config.check_signal({0: 1}) == True
