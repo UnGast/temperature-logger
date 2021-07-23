cbe8ae36e5daaa3eb45fffaa150d27f4f24d60ec
UnGast <zadrian@web.de>
2021-07-23 22:42:22 +0200

    improve mock sensor config options

--- backend/sensors/Mock.py
+++ backend/sensors/Mock.py
@@ -1,12 +1,14 @@
 import math
-from datetime import datetime
+import time
 import sensors
 
 class Mock(sensors.Sensor):
-  def __init__(self, id, position, correction_offset):
+  def __init__(self, id, position, amplitude, mean, period, correction_offset):
         super().__init__(id=id, type="Mock", position=position, accuracy=0.5, correction_offset=correction_offset)
+        self.amplitude = amplitude
+        self.mean = mean
+        self.period = period
 
   def __read__(self) -> float:
-        time = datetime.now()
-        value = round(math.sin(time.timestamp() * 0.3 + self.id) * 20, 1)
+        value = self.amplitude * math.sin((time.time() % self.period) / self.period * 2 * 3.1415926535) + self.mean
         return value
